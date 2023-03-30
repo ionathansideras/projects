5a2f87371467c234e3a2d54ca5c2330c0857dea3
Ionathan Sideras <119215357+ionathansideras@users.noreply.github.com>
2023-03-31 01:35:18 +0300

    Add files via upload
    shelter

---
+++ shelter/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    form: './src/form.js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+        
+        {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        },
+     ],
+    },
+};
